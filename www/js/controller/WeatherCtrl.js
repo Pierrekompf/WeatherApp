7551e8253f18d0acb600c5285cf13fb5f6c13f35
tcrepy <tanguycrepy@gmail.com>
2017-02-27 23:42:09 +0100

    création d'icon pour le menu fixé en bas. Création de class pour les icons de temps.

--- www/js/controller/WeatherCtrl.js
+++ www/js/controller/WeatherCtrl.js
@@ -3,7 +3,7 @@ function WeatherCtrl($scope, $http, GeolocationService){
     $scope.panel = 0;
 
     $scope.search = function(){
-        var url="http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.city +"&mode=json&cnt=10&units=metric&APPID=29c40f0944c926eab4d44dd6827a444e";
+        var url="http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.city +"&mode=json&cnt=10&units=metric&lang=fr&APPID=29c40f0944c926eab4d44dd6827a444e";
         $scope.loader = true;
         $http.get(url).success(httpSuccess).error(httpError);
     }
@@ -16,7 +16,7 @@ function WeatherCtrl($scope, $http, GeolocationService){
     $scope.geolocate = function () {
         GeolocationService.getCurrentPosition(function (position) {
             $scope.loader = true;
-            $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat='+ position.coords.latitude +'&lon='+ position.coords.longitude +'&mode=json&cnt=10&units=metric&APPID=29c40f0944c926eab4d44dd6827a444e')
+            $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?lat='+ position.coords.latitude +'&lon='+ position.coords.longitude +'&mode=json&cnt=10&units=metric&lang=fr&APPID=29c40f0944c926eab4d44dd6827a444e')
                 .success(httpSuccess).error(httpError);
         }, function () {
             alert('Impossible de récupérer votre position');
